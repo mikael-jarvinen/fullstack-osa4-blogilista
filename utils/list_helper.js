80ed2f0b5dfa39bd0128348b811d928934fb9cf9
usermine12 <mikaeljarv@gmail.com>
2020-04-02 21:03:44 +0300

    osa4 4.7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -35,8 +35,6 @@ const mostBlogs = blogs => {
     scoreboard[authors.indexOf(blog.author)]++
   })
 
-  console.log('debugging', authors, scoreboard)
-
   let maxIndex = 0
   for (let i=0; i< scoreboard.length; i++){
     if(scoreboard[i] > scoreboard[maxIndex]){
@@ -50,9 +48,33 @@ const mostBlogs = blogs => {
   }
 }
 
+const mostLikes = blogs => {
+  let authors = blogs.map(blog => blog.author)
+  authors = lodash.uniq(authors)
+  let scoreboard = []
+  authors.forEach(() => scoreboard = scoreboard.concat(0))
+
+  blogs.forEach(blog => {
+    scoreboard[authors.indexOf(blog.author)] += blog.likes
+  })
+
+  let maxIndex = 0
+  for (let i=0; i< scoreboard.length; i++){
+    if(scoreboard[i] > scoreboard[maxIndex]){
+      maxIndex = i
+    }
+  }
+
+  return {
+    author: authors[maxIndex],
+    likes: scoreboard[maxIndex]
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
-  mostBlogs
+  mostBlogs,
+  mostLikes
 }
