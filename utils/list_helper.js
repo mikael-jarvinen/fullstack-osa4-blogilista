4653d3485fc3e314f6c38556d751d56b2fb0a1ce
usermine12 <mikaeljarv@gmail.com>
2020-04-02 19:01:25 +0300

    osa4 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,7 +9,26 @@ const totalLikes = blogs => {
   return blogs.reduce(sum ,0)
 }
 
+const favoriteBlog = blogs => {
+  return blogs.reduce((fav, blog) => {
+    if (blog.likes > fav.likes){
+      return {
+        title: blog.title,
+        author: blog.author,
+        likes: blog.likes
+      }
+    } else {
+      return {
+        title: fav.title,
+        author: fav.author,
+        likes: fav.likes
+      }
+    }
+  }, {likes:0})
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
