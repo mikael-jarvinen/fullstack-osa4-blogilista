e9fd12f45832fea6cfe0a9a554f2b8a48fb206a6
usermine12 <mikaeljarv@gmail.com>
2020-04-02 20:45:57 +0300

    osa4 4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodash = require('lodash')
+
 const dummy = blogs => {
   return 1
 }
@@ -18,17 +20,39 @@ const favoriteBlog = blogs => {
         likes: blog.likes
       }
     } else {
-      return {
-        title: fav.title,
-        author: fav.author,
-        likes: fav.likes
-      }
+      return fav
     }
   }, {likes:0})
+}
+
+const mostBlogs = blogs => {
+  let authors = blogs.map(blog => blog.author)
+  authors = lodash.uniq(authors)
+  let scoreboard = []
+  authors.forEach(() => scoreboard = scoreboard.concat(0))
+
+  blogs.forEach(blog => {
+    scoreboard[authors.indexOf(blog.author)]++
+  })
+
+  console.log('debugging', authors, scoreboard)
+
+  let maxIndex = 0
+  for (let i=0; i< scoreboard.length; i++){
+    if(scoreboard[i] > scoreboard[maxIndex]){
+      maxIndex = i
+    }
+  }
+
+  return {
+    author: authors[maxIndex],
+    blogs: scoreboard[maxIndex]
+  }
 }
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
