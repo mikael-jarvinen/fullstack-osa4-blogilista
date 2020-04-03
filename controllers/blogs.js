9671ef85435c6867eba761428a5af0d436a0b984
usermine12 <mikaeljarv@gmail.com>
2020-04-04 02:41:56 +0300

    osa4 4.20

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -4,14 +4,6 @@ const logger = require('../utils/logger')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if(authorization && authorization.toLowerCase().startsWith('bearer')){
-    return authorization.substring(7)
-  }
-  return null
-}
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1})
   response.json(blogs.map(blog => blog.toJSON()))
@@ -19,7 +11,7 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
-  const token = getTokenFrom(request)
+  const token = request.token
 
   const decodedToken = jwt.verify(token, process.env.SECRET)
   if (!token || !decodedToken.id) {
