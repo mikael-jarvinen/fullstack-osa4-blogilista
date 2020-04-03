10364c6fb436334bf3caf7b0b0782378868b7418
usermine12 <mikaeljarv@gmail.com>
2020-04-04 01:48:25 +0300

    osa4 4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,14 +1,17 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const logger = require('../utils/logger')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1})
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
+
+  const user = await User.findById(body.userId)
 
   if(!body.title || !body.url){
     return response.status(400).end()
@@ -18,7 +21,8 @@ blogsRouter.post('/', async (request, response, next) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: user._id
   })
 
   if(!body.likes){
@@ -26,7 +30,8 @@ blogsRouter.post('/', async (request, response, next) => {
       title: body.title,
       author: body.author,
       url: body.url,
-      likes: 0
+      likes: 0,
+      user: user._id
     })
   }
 
@@ -34,6 +39,8 @@ blogsRouter.post('/', async (request, response, next) => {
 
   try {
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
     response.json(savedBlog.toJSON())
   } catch (e) {
     next(e)
--- controllers/users.js
+++ controllers/users.js
@@ -37,6 +37,7 @@ usersRouter.post('/', async (request, response, next) => {
 usersRouter.get('/', async (request, response) => {
   const users = await User
     .find({})
+    .populate('Note', { title: 1, author: 1 })
 
   response.json(users.map(u => u.toJSON()))
 })
