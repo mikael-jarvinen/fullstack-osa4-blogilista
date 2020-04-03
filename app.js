98af3c37a185a7479a7366456f682a36b54a2261
usermine12 <mikaeljarv@gmail.com>
2020-04-04 02:01:03 +0300

    osa4 4.18

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const loginRouter = require('./controllers/login')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -24,6 +25,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
